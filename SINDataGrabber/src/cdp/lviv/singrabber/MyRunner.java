0fc38573f229071582a436969d710149261d1a2d
novostavsky <novostavsky@gmail.com>
2012-03-13 21:21:58 +0200

    moved search input to "main"

--- SINDataGrabber/src/cdp/lviv/singrabber/MyRunner.java
+++ SINDataGrabber/src/cdp/lviv/singrabber/MyRunner.java
@@ -3,7 +3,11 @@ package cdp.lviv.singrabber;
 import java.sql.SQLException;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.PageFactory;
 
 import cdp.lviv.singrabber.dao.Tester;
@@ -26,25 +30,23 @@ public class MyRunner {
 		WebDriver driver = new HtmlUnitDriver();
 		
 		try {
-			MyRunner.grab(driver);
+			MyRunner.grab(driver, "Lviv QA", "login", "passwd");
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
-        driver.quit();
 	}
 	
-	public static void grab(WebDriver driver) throws InterruptedException{
+	public static void grab(WebDriver driver, String searchString, String login, String passwd) throws InterruptedException{
 		TesterManager manager = new TesterManager();
 		
 		LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
         loginPage.open();
-		loginPage.login("", "");
+		loginPage.login(login, passwd);
 		
 		HomePage homePage = PageFactory.initElements(driver, HomePage.class);
-		homePage.search("Lviv test");
+		homePage.search(searchString);
 		
-		for(int i=1; i<10; i++){
+		for(int i=1; i<11; i++){
 			SearchResultPage searchResult = PageFactory.initElements(driver, SearchResultPage.class);
 
 			for(int j=1; j<11; j ++){
